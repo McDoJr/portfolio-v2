1aacd90315ec7cb8c7d4e4842bfefc52b8986dfa
Karl Bejerano <51730379+McDoJr@users.noreply.github.com>
2025-12-12 22:37:51 +0800

    Optimized codes

--- app/page.tsx
+++ app/page.tsx
@@ -25,6 +25,7 @@ export default function Page() {
             about: aboutRef,
             tech: techRef,
             experience: experienceRef,
+            projects: projectRef
         },
         clicked,
         { offset: 120 }
