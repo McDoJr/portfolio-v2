2ca9db2917bac05f8d6015419333aad4ffd97f09
Karl Bejerano <51730379+McDoJr@users.noreply.github.com>
2025-12-17 22:03:12 +0800

    Added DentWise project

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import About from "@/components/About";
 import Experience from "@/components/Experience";
@@ -11,43 +11,42 @@ import { useScrollSpy } from "@/hooks/useScrollSpy";
 import { useRef, useState } from "react";
 
 export default function Page() {
+  const rightRef = useRef<HTMLDivElement>(null);
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const techRef = useRef<HTMLDivElement>(null);
+  const experienceRef = useRef<HTMLDivElement>(null);
+  const projectRef = useRef<HTMLDivElement>(null);
+  const [clicked, setClicked] = useState(false);
 
-    const rightRef = useRef<HTMLDivElement>(null);
-    const aboutRef = useRef<HTMLDivElement>(null);
-    const techRef = useRef<HTMLDivElement>(null);
-    const experienceRef = useRef<HTMLDivElement>(null);
-    const projectRef = useRef<HTMLDivElement>(null);
-    const [clicked, setClicked] = useState(false);
+  const [view, setView] = useScrollSpy(
+    rightRef,
+    {
+      about: aboutRef,
+      tech: techRef,
+      experience: experienceRef,
+      projects: projectRef,
+    },
+    clicked,
+    { offset: 120 },
+  );
 
-    const [view, setView] = useScrollSpy(
-        rightRef,
-        {
-            about: aboutRef,
-            tech: techRef,
-            experience: experienceRef,
-            projects: projectRef
-        },
-        clicked,
-        { offset: 120 }
-    );
-
-    return (
-        <ViewProvider values={{ view, setView, setClicked }}>
-            <main className="w-full first:h-screen grid grid-cols-1 first:grid-cols-2 overflow-auto first:overflow-hidden bg-primary">
-                <Sidebar rightRef={rightRef} />
-                <div
-                    ref={rightRef}
-                    className="flex flex-col items-center first:items-start pb-20 first:py-24 first:overflow-y-auto"
-                >
-                    <div className="max-w-[675px] lg:w-[675px] flex flex-col px-6 lg:px-0 font-outfit">
-                        <About ref={aboutRef} id="about" />
-                        <TechStack ref={techRef} id="tech" />
-                        <Experience ref={experienceRef} id="experience" />
-                        <Projects ref={projectRef} id="projects" />
-                        <Footer />
-                    </div>
-                </div>
-            </main>
-        </ViewProvider>
-    )
+  return (
+    <ViewProvider values={{ view, setView, setClicked }}>
+      <main className="w-full first:h-screen grid grid-cols-1 first:grid-cols-2 overflow-auto first:overflow-hidden bg-primary">
+        <Sidebar rightRef={rightRef} />
+        <div
+          ref={rightRef}
+          className="flex flex-col items-center first:items-start pb-20 first:py-24 first:overflow-y-auto"
+        >
+          <div className="max-w-[675px] lg:w-[675px] flex flex-col px-6 lg:px-0 font-outfit">
+            <About ref={aboutRef} id="about" />
+            <TechStack ref={techRef} id="tech" />
+            <Experience ref={experienceRef} id="experience" />
+            <Projects ref={projectRef} id="projects" />
+            <Footer />
+          </div>
+        </div>
+      </main>
+    </ViewProvider>
+  );
 }
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,68 +2,81 @@ import { projects } from "@/utils/datas";
 import { twMerge } from "tailwind-merge";
 
 type ProjectsProps = React.HTMLAttributes<HTMLDivElement> & {
-    ref?: React.Ref<HTMLDivElement>;
+  ref?: React.Ref<HTMLDivElement>;
 };
 
 export default function Projects(props: ProjectsProps) {
-
-    return (
-        <div
-            className="flex flex-col gap-6 pb-8 first:pb-40 text-2xl lg:text-[18px] text-neutral-400 font-light"
-            {...props}
-        >
-            <h1 className="inline text-lg text-neutral-300 font-medium mb-6 first:hidden">PROJECTS</h1>
-            {projects.map(project => (
-                <ProjectCard {...project} key={project.title} />
-            ))}
-        </div>
-    )
+  return (
+    <div
+      className="flex flex-col gap-6 pb-8 first:pb-40 text-2xl lg:text-[18px] text-neutral-400 font-light"
+      {...props}
+    >
+      <h1 className="inline text-lg text-neutral-300 font-medium mb-6 first:hidden">
+        PROJECTS
+      </h1>
+      {projects.map((project) => (
+        <ProjectCard {...project} key={project.title} />
+      ))}
+    </div>
+  );
 }
 
 export type ProjectCardProps = {
-    title: string;
-    description: string;
-    thumbnail: string;
-    techs: string[];
-}
+  title: string;
+  description: string;
+  thumbnail: string;
+  techs: string[];
+  url?: string;
+};
 
-function ProjectCard({ title, description, thumbnail, techs }: ProjectCardProps) {
-    return (
-        <div
-            className={twMerge(
-                "w-full py-4 rounded flex flex-col sm:flex-row gap-6",
-                "hover:px-4 hover:bg-white/3 transition-all hover:shadow-md cursor-pointer"
-            )}
-        >
-            <span className="text-[17px] block sm:hidden text-neutral-300 font-[450]">
-                {title}
-            </span>
-            <div className="w-full sm:w-40 sm:h-[100px] rounded border-2 border-neutral-600">
-                <img className="w-full h-full" src={thumbnail} alt="thumbnail" />
-                {/* <Image src={thumbnail} alt="thumbnail" fill className="object-cover rounded" /> */}
-            </div>
+function ProjectCard({
+  title,
+  description,
+  thumbnail,
+  techs,
+  url,
+}: ProjectCardProps) {
+  return (
+    <a
+      href={url ?? "#"}
+      target={url ? "_blank" : undefined}
+      onClick={(e) => {
+        if (!url) e.preventDefault();
+      }}
+      className={twMerge(
+        "w-full py-4 rounded flex flex-col sm:flex-row gap-6",
+        "hover:px-4 hover:bg-white/3 transition-all hover:shadow-md cursor-pointer",
+      )}
+    >
+      <span className="text-[17px] block sm:hidden text-neutral-300 font-[450]">
+        {title}
+      </span>
+      <div className="w-full sm:w-40 sm:h-[100px] rounded border-2 border-neutral-600">
+        <img className="w-full h-full" src={thumbnail} alt="thumbnail" />
+        {/* <Image src={thumbnail} alt="thumbnail" fill className="object-cover rounded" /> */}
+      </div>
 
-            <div className="flex-1 flex flex-col gap-3">
-                <span className="text-[17px] hidden sm:block text-neutral-300 font-[450]">
-                    {title}
-                </span>
+      <div className="flex-1 flex flex-col gap-3">
+        <span className="text-[17px] hidden sm:block text-neutral-300 font-[450]">
+          {title}
+        </span>
 
-                <p className="text-[16px]">{description}</p>
+        <p className="text-[16px]">{description}</p>
 
-                <div className="flex flex-wrap gap-3 items-center">
-                    {techs.map((tech) => (
-                        <Tag key={tech} tag={tech} />
-                    ))}
-                </div>
-            </div>
+        <div className="flex flex-wrap gap-3 items-center">
+          {techs.map((tech) => (
+            <Tag key={tech} tag={tech} />
+          ))}
         </div>
-    )
+      </div>
+    </a>
+  );
 }
 
 function Tag({ tag }: { tag: string }) {
-    return (
-        <div className="py-1 px-2 rounded-full bg-[#c7cbcb]">
-            <p className="text-[14px] text-[#150759]">{tag}</p>
-        </div>
-    )
+  return (
+    <div className="py-1 px-2 rounded-full bg-[#c7cbcb]">
+      <p className="text-[14px] text-[#150759]">{tag}</p>
+    </div>
+  );
 }
